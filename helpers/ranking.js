865220a642a20ccb6e6d6a76044647e190aa7e05
Amine Barchid <amine.barchid@gmail.com>
2020-10-10 23:04:48 +0100

    commentcount into ranking

--- helpers/ranking.js
+++ helpers/ranking.js
@@ -3,14 +3,17 @@ const rankFeed = (posts) => {
     // algorithm arguments
     // likes
     const l = post.likeCount;
+    // comments
+    const c = post.commentCount;
     // time since submission (in hours)
     const t =
       (new Date().getTime() - new Date(post.createdAt).getTime()) /
       (36 * 100000);
     // gravity constant
     const g = 1.8;
-    post.score = l / Math.pow(t + 2, g);
+    post.score = (l + (c*0.05) + 0.75) / Math.pow(t + 2, g);
   });
+  console.log(posts);
   return posts.sort((a, b) => b.score - a.score).slice(0, 20);
 };
 
